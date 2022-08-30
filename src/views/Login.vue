ad83ecb8900ef155213a255a31b83c8ebf4a3a8c
Paulymatos <paulymatos3@gmail.com>
2022-08-30 08:53:55 -0300

    Atualizada tela de Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,23 @@
 <template>
    <div class="main">
-    <p>Login</p>
+      <div class="componente-login">
+    <h1>Login</h1>
     <p>Digite seu Login e Senha</p>
-    <input type="text"  placeholder="Nome ou e-mail">
+   <button>Nome ou e-mail</button>
     <br>
     <br>
-    <input type="text"  placeholder="Senha">
-    <p>Esqueceu a Senha?</p>
-    <p>Login</p>
-    <div class="google">
-    <input type="text" placeholder=" sign-in with Google">
+    <button> Senha </button>
+    <h5> Esqueceu a Senha? </h5>
+    <div class="login">
+    <button>Login</button>
     </div>
-    <p>Não é um membro ainda?Cadastre-se</p>
+    <br>
+    <br>
+    <button> sign-in with Google </button> 
+    <div class="cadastra">
+    <p>Não é um membro ainda? <span>Cadastre-se</span></p>
+    </div>
+    </div>
    </div>
 </template>
 
@@ -23,11 +29,85 @@ export default {
 
 <style scoped>
 
+
+body{
+ background-color: black;
+
+}
+
 .main{
    font-family: 'Roboto Mono', monospace;
-  
+   display: flex;
+   margin: auto;
+   background-color: black;
+   align-items: center;
+   justify-content: center;
+   color: aliceblue;
+   width: 100%;
+   height: 100vh; 
 }
+.main p{
+   font-size: 15px
+}
+.main h5{
+   font-size:10px;
+   font-family: italic;
+   margin-left:30%
+}
+.componente-login{
+margin-top: 3%;
+margin-right:5% ;
+text-align: center;
+width: 515px;
+height: 576px;
+
+}
+input{
+  width: 50%;
+  height: 5%;
+  border: 1px solid white;
+  border-radius: 10px;
+  background-color: black;
+}
+
 .google{
    font-family: 'PT Sans', sans-serif;
+   margin: auto;
+   text-align: center;
+ 
 }
+button{
+   width: 50%;
+ 
+   padding: 10px;
+   border: 1px solid white;
+   border-radius: 10px;
+   background-color:black ;
+   color: white;
+   text-align: left;
+   font-size: 10px;
+}
+.login button{  
+   border: 1px solid #378BED;
+   border-radius: 10px;
+   padding: 10px;
+   margin: auto;
+   text-align: center;
+   background-color:black;
+   color:#378BED ;
+     
+}
+.cadastra{
+   justify-content: space-between ;
+}
+.cadastra p{
+   font-family: italic;
+   font-size: 10px;
+
+   
+}
+.cadastra span{
+   color:#378BED  ;
+}
+
 </style>
