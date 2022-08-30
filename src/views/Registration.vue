53cbe26b1a5b76c87cbf3737891506149c945d51
Lucas Graf <103954177+GrafLucas@users.noreply.github.com>
2022-08-30 10:45:19 -0300

    Update Registration.vue

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,12 +1,29 @@
 <template>
-<h1> registration </h1>
+    <div class="main">
+    <h1>Cadastre-se</h1>
+       <p>Preencha os campos corretamente</p>
+    <div>
+        <input type="text" placeholder="Digite seu e-mail">
+    </div>
+    <div>
+        <input type="text" placeholder="Digite seu user name">
+    </div>
+    <div>
+        <input type="password" placeholder="Digite sua senha">
+    </div>
+    <div>
+        <input type="password" placeholder="Confirme sua senha">
+    </div>
+    <p>Cadastrar</p>
 
+
+    </div>
 </template>
-<script>
-export default {
-  
-}
-</script>
-<style scoped>
 
+
+<style scoped>
+.main{
+   margin: auto 0;
+
+}
 </style>
