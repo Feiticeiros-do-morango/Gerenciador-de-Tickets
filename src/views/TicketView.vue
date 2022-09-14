1e48ce94c8f8df3060f678908b3eb92e6d99a60a
Paulymatos <paulymatos3@gmail.com>
2022-09-14 11:22:57 -0300

    Trabalhando a tela TicketView

--- src/views/TicketView.vue
+++ src/views/TicketView.vue
@@ -1,18 +1,30 @@
 <template>
 <div class="container">   
 <div class="sectionA">
+  <div class="inicio">
    <h1>NOVO TICKET</h1>
+   <button>Branches</button>
+   </div>
+   <div class="tech">
     <p>Dev/Javascript</p>
+    </div>
   <p>ID nº </p>
   <p>Aberto em : 10/08/2022</p>
   <p>Data limite: 21/08/2022 </p>
-  <select name="criado" id="">Criado por</select>
+  <div class="criado">
+ <p>Criado por</p> 
+ </div>
   <div class="quadrado">
     <p>Descrição</p>
-    <span>Anexar Arquivo</span>
+    <div class="quadradob">
+    <p>Anexar Arquivo</p>
+    </div>
   </div>
     </div>
 <div class="sectionB">
+  <p>Colaboradores</p>
+  <p>Comentários...</p>
+  <p>Fechado em: 20/08/2022</p>
 
 </div>
     </div>
@@ -30,12 +42,23 @@ export default {
     height: 94vh;
     background-image: url('../assets/backgroundTicktView.jpg');
     background-size:205vh;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    box-shadow: inset 0px 4px 4px rgba(184, 182, 182, 0.25);
     border-radius: 30px 30px 0px 0px;
     display: grid;
     grid-template-columns: 8fr 4fr ;
     grid-template-areas: "sectionA sectionB";
-   
+  
+}
+.inicio{
+  display: flex;
+  gap:50vh;
+  color: black
+}
+.inicio button{
+  border-radius: 20px;
+  width: 15vh;
+  border:none;
+  background-color:rgba(186, 183, 183, 0.762)
   
 }
 
@@ -44,6 +67,11 @@ export default {
      grid-area:sectionA;
      padding:2vh;
      margin-left:4vh;
+     color:black;
+     
+}
+.tech{
+  color: gray;
 }
 .quadrado{
 background: rgb(207, 206, 206);
@@ -52,10 +80,22 @@ height: 50%;
 margin-top:4vh;
 border-radius: 10px;
 }
+.quadrado p{
+  padding: 1vh;
+}
+.quadradob p{
+  text-align: left;
+  padding-top: 35vh;
+}
 .sectionB{
-    color: gray;
+    color: rgba(186, 186, 186, 0.504);
     grid-area:sectionB;
+    color:black;
+    display: flex;
+    flex-direction: column;
+    gap:20vh
 
 }
+
 
 </style>
