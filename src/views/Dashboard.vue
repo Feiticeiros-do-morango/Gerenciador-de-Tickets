ab8fb3d22006998d173b62337f07d29cd71bdfd9
Júlia <julia.nog.sou@gmail.com>
2022-10-04 11:25:22 -0300

    dashboard progress

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,19 +6,19 @@
     <div class="dashboard">
 
       <grid-layout :layout.sync="layout" :col-num="colNum" :row-height="30" :is-draggable="draggable"
-        :is-resizable="resizable" :vertical-compact="true" :use-css-transforms="true">
-        <grid-item v-for="item in layout" :static="item.static" :x="item.x" :y="item.y" :w="item.w" :h="item.h"
-          :i="item.i" :titulo="item.titulo" :key="item.i" >
-          <span class="text"  >
-            <div class="minTop">
-              <p>{{item.titulo}}</p>
-              <p>{{item.i}}</p>
-            </div>
-            <div class="minBottom">
-              <p>Tech/Tech</p>
-            </div>
+        :is-resizable="resizable" :vertical-compact="true" :use-css-transforms="true"
+        :style="{width: size + 'px', height: size + 'px'}">
+        <grid-item v-for="item in layout" :static="item.static" :x="item.x" :y="item.y" :w="item.w"
+          :h="item.h" :i="item.i" :titulo="item.titulo" :key="item.i"
+          v-bind:class="getPrioridade(item.prioridade)">
+          <span class="text">
+            <svg viewBox="0 0 250 250" xmlns="http://www.w3.org/2000/svg">
+              <text id="tituloText" x="1" y="20" >{{item.titulo}}</text>
+              <text id="idText" x="1" y="35" fill="gray" >{{item.i}}</text>
+              <text id="techText" x="1" y="80" >{{item.tech}}</text>
+            </svg>
           </span>
-          <span class="remove" @click="removeItem(item.i)">x</span>
+          <span class="remove"  @click="removeItem(item.i)">x</span>
         </grid-item>
       </grid-layout>
     </div>
@@ -26,9 +26,12 @@
 </template>
 
 <script>
-
+import { db, auth } from "../Firebase/index"
+import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 import { GridLayout, GridItem } from "vue-grid-layout"
 import Menu from "@/components/Menu.vue";
+
+
 export default {
   components: {
     Menu, GridLayout, GridItem
@@ -36,45 +39,58 @@ export default {
   data() {
     return {
       layout: [
-        { x: 0, y: 0, w: 2, h: 3, i: "132132", titulo: "Main ticket" },
-        { x: 2, y: 0, w: 2, h: 3, i: "1254125", titulo: "Main ticket" },
-        { x: 4, y: 0, w: 2, h: 3, i: "2", titulo: "Main ticket" },
-        { x: 6, y: 0, w: 2, h: 3, i: "3", titulo: "Main ticket" },
-        { x: 8, y: 0, w: 2, h: 3, i: "4", titulo: "Main ticket" },
-        
+        // { x: 0, y: 0, w: 2, h: 3, i:"11234567", titulo:"", tech:"" }
+
       ],
       draggable: true,
       resizable: true,
       colNum: 10,
       index: 0,
+      
     }
   },
   mounted() {
     // this.$gridlayout.load();
     this.index = this.layout.length;
+    this.addItem()
+
 
   },
   methods: {
-    addItem: function () {
-      // Add a new item. It must have a unique key!
-      this.layout.push({
-        x: (this.layout.length * 2) % (this.colNum || 12),
-        y: this.layout.length + (this.colNum || 12), // puts it at the bottom
-        w: 2,
-        h: 3,
-        i: this.index,
-        titulo: "banana"
+    addItem: async function () {
+
+      const querySnapshot = await getDocs(collection(db, "ticket"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+
+        this.layout.push({
+          x: (this.layout.length * 2) % (this.colNum || 12),
+          y: this.layout.length + (this.colNum || 12), // puts it at the bottom
+          w: 2,
+          h: 3,
+          i: doc.id,
+          titulo: doc.data().titulo,
+          tech: doc.data().tecnologia,
+          prioridade: doc.data().prioridade,
+          tipo: doc.data().tipo,
+        });
+
+        console.log(doc.id, " => ", doc.data());
       });
-      // Increment the counter to ensure key is always unique.
-      this.index++;
     },
+
     removeItem: function (val) {
       const index = this.layout.map(item => item.i).indexOf(val);
       this.layout.splice(index, 1);
     },
+    getPrioridade: function (prioridade){
+      if ( prioridade == "naoUrgente"){
+        return "naoUrgente"
+      }
+
+    }
   }
 }
-
 </script>
 
 <style scoped>
@@ -93,21 +109,21 @@ export default {
 }
 
 .dashboard {
-height: 90vh;
-width: 100%;
-padding-top: 2vh;
+  height: 90vh;
+  width: 100%;
+  padding-top: 2vh;
 
   /* Background*/
-background-color:rgba(150, 150, 150, 0.863);
-background-image: linear-gradient(rgba(255, 255, 255, 0) 2px, transparent 2px),
-linear-gradient(90deg, rgba(255, 255, 255, 0) 2px, transparent 2px),
-linear-gradient(rgb(49, 49, 49) 1px, transparent 1px),
-linear-gradient(90deg, rgb(49, 49, 49) 1px, transparent 1px);
-background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
-background-position:-2px -2px, -2px -2px, -1px -1px, -1px -1px;
+  background-color: rgba(150, 150, 150, 0.863);
+  background-image: linear-gradient(rgba(255, 255, 255, 0) 2px, transparent 2px),
+    linear-gradient(90deg, rgba(255, 255, 255, 0) 2px, transparent 2px),
+    linear-gradient(rgb(49, 49, 49) 1px, transparent 1px),
+    linear-gradient(90deg, rgb(49, 49, 49) 1px, transparent 1px);
+  background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
+  background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
 
-box-shadow: inset 3px 5px 10px rgba(0, 0, 0, 0.705);
-border-radius: 30px 30px 0px 0px;
+  box-shadow: inset 3px 5px 10px rgba(0, 0, 0, 0.705);
+  border-radius: 30px 30px 0px 0px;
 
 }
 
@@ -132,23 +148,34 @@ Configuração dos cards
 .vue-grid-item:not(.vue-grid-placeholder) {
   background: rgba(255, 255, 255, 0.575);
   backdrop-filter: blur(5px);
-  border-left: solid 10px rgb(23, 172, 122);
   border-radius: 15px;
   padding: 5px;
   overflow: hidden;
 }
 
-.minTop p{
-  font-size: 2vmin;
-  margin-top: 1vh;
-  margin-left: 0.5vw;
-  text-align: left;
+.naoUrgente{
+  border-left: solid 11px rgb(23, 172, 122);
+}
+.poucoUrgente{
+  border-left: solid 11px rgb(23, 172, 122);
+}
+.urgente{
+  border-left: solid 11px rgb(23, 172, 122);
+}
+.muitoUrgente{
+  border-left: solid 11px rgb(23, 172, 122);
+}
+.emergencia{
+  border-left: solid 11px rgb(23, 172, 122);
 }
 
-.minBottom p{
-  font-size: 18px;
-  margin-left: 0.5vw;
-  text-align: left;
+
+#tituloText{
+  font-size: 120%;
+}
+
+#idText{
+  font-size: 60%;
 }
 
 .vue-grid-item .resizing {
