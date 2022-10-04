b46385d0dd27d5a0ae18c005d90cb8e1b8c5903d
Júlia <julia.nog.sou@gmail.com>
2022-10-04 11:25:36 -0300

    ajuste no modal

--- src/views/CriarTicket.vue
+++ src/views/CriarTicket.vue
@@ -1,90 +1,146 @@
 <template>
     <div>
-    
-<Button label="Show" icon="pi pi-external-link" @click="openModal" />
-<Dialog header="Novo Ticket" :visible.sync="displayModal" :containerStyle="{width: '50vw'}" :modal="true">
-<section>
-  <form>
- 
-            <div>
-            <label for=""> Assunto </label>
-            <input type="text" id="nome" placeholder="">
-           </div>
-           <div>
-            <label for="">Tecnologia</label>
-            <input type="text" id="tec" placeholder="">
-           </div>
-           <div>
-            <label for="">Colaboradores</label> 
-         
+
+        <Button label="Show" icon="pi pi-external-link" @click="openModal" />
+        <Dialog header="Novo Ticket" :visible.sync="displayModal" :containerStyle="{width: '50vw'}" :modal="true">
+           
+                <section>
+                    <form>
+                        <div class="create">
+                        <div class="a">
+                            <label for=""> Assunto </label>
+                            <input type="text" id="nome" placeholder="">
+                        </div>
+                        <div>
+                            <label for="">Tecnologia</label>
+                            <input type="text" id="tec" placeholder="">
+                        </div>
+                        <div>
+                            <label for="">Colaboradores</label>
+                            <select name="" id=""></select>
+
+                        </div>
+                   
+                        <div>
+                            <label for=""> Data limite </label>
+                            <input type="text" id="nome" placeholder=" ">
+                        </div>
+                        <div>
+                            <label for=""> Criado por </label>
+                            <input type="text" id="nome" placeholder=" ">
+                        </div>
+                        <div>
+                            <label for=""> Prioridade </label>
+                            <select name="" id=""></select>
+                        </div>
+                        <div>
+                            <label for=""> Tipo</label>
+                            <select name="" id=""></select>
+                        </div>
+                        <div class="enviar">
+                            <input type="submit" value="Enviar">
+                        </div>
+                   
             </div>
-            <div>
-            <label for=""> Aberto em </label>
-            <input type="text" id="nome" placeholder=" ">
-            </div>
-            <div>
-            <label for=""> Data limite </label>
-            <input type="text" id="nome" placeholder=" ">
-            </div>
-            <div>
-            <label for=""> Criado por </label>
-            <input type="text" id="nome" placeholder=" ">
-          </div>
-          <div>
-             <input type="submit" value="Enviar">
-          </div>
-            </form>
-            </section>
+            <!-- <div class="ticket">
+                <div class="minTop">
+                    <p>Título-Título-Título-</p>
+                    <p>000000000000</p>
+                </div>
+                <div class="minbotton">
+                    <p>Tecnologia/Tecnologia</p>
+                </div>   
+            </div> -->
+        </form>
+        </section>
         </Dialog>
-        
-        <Button label="No" icon="pi pi-times" @click="closeModal" class="p-button-text"/>
+
+
+
+        <Button label="No" icon="pi pi-times" @click="closeModal" class="p-button-text" />
         <Button label="Yes" icon="pi pi-check" @click="closeModal" autofocus />
-   
+
     </div>
 
 
 </template>
 
 <script>
- import Dialog from 'primevue/dialog';
+import Dialog from 'primevue/dialog';
 export default {
     components: { Dialog },
     data() {
         return {
             displayModal: true
         }
-},
-methods: {
-    openModal() {
+    },
+    methods: {
+        openModal() {
             this.displayModal = true;
         },
         closeModal() {
             this.displayModal = false;
-    
+
+        }
     }
-} 
 }
 
 </script>
 
 <style scoped>
-*{
-  margin:0;
-  padding: 0;
+* {
+    margin: 0;
+    padding: 0;
 }
-section{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 60vh;
+
+section {
+    display: flex;
+    flex-direction: row;
+    height: 40vh;
+    width: 40vw;
+    /* grid-template-columns: 6fr 6fr;
+    grid-template-areas: "form ticket"; */
+
 }
-form div{
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 5px;
+
+/* .create {
+    grid-area: form;
+    display: flex;
+    flex-direction: column;
+   width: 80%;
 }
-form input{
-  outline: unset ;
+
+.ticket {
+    grid-area: ticket;
+    display: flex;
+    flex-direction: column;
+     width: 80%;
+} */
+
+form div {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 5px;
+}
+
+form input {
+    outline: unset;
+    background-color: rgb(201, 200, 200);
+    border: none;
+    border-radius: 6px;
+    color: white;
+    padding: 0.25vh;
+    font-weight: bold;
+}
+
+form select {
+    outline: unset;
+    background-color: rgb(201, 200, 200);
+    border: none;
+    border-radius: 6px;
+    color: white;
+    padding: 0.25vh;
+    font-weight: bold;
 }
 
 .p-button {
@@ -102,72 +158,17 @@ p {
     justify-content: center;
 }
 
-/* .container{
-     font-family: 'Roboto Mono', monospace;
-    background-color: #0a0a33;
-    color: aliceblue;
-    width: 100%;
-    height: 100%;
-    padding: 2vw;
-}
-.dados {
-  background-color: rgb(185, 181, 181);
-  width: 50%;
-  height: 40%;
-  border-radius: 10px;
-  padding-left: 1vw;
-}
-.dados h1 {
-  font-size: 20px;
-  margin-bottom: 2vh;
-  padding-top: 2vh;
-  border-bottom: 2px solid rgb(243, 240, 240);
-}
-
-.dados label {
-  width: 60%;
-  height: 60%;
-}
-input:focus {
-  outline: none;
-}
-.dados input {
-  display: flex;
-  width: 50%;
-  height: 3vh;
-  border: none;
-  border-radius: 6px;
-  background-color: rgb(158, 156, 156);
-}
-.dados select {
-  border: none;
-  border-radius: 4px;
-  background-color: rgb(158, 156, 156);
-  margin-left: 1vh;
-}
-.periodos {
-  background-color: rgb(185, 181, 181);
-  width: 50%;
-  height: 50%;
-  margin-top: 2vh;
-  border-radius: 10px;
-  padding-left: 1vw;
-}
-.periodos h1 {
-  font-size: 20px;
-  margin-bottom: 5vh;
-  padding-top: 2vh;
-  border-bottom: 2px solid rgb(243, 240, 240);
-}
-.periodos input{
+.enviar {
     display: flex;
-    width: 50%;
-    height:3vh;
-    border: none;
-    border-radius: 5px;
-    background-color: rgb(158, 156, 156);
-} */
+    align-items: center;
+    justify-content: center;
+    width: 8vw;
+    padding-top: 2vh;
+    color: white;
+}
 
-
-
+.enviar input {
+    background-color: #5c677d;
+    padding: 1vh;
+}
 </style>
