795dabd3fa2dd7077314bee3fbb6dd873faff5c0
Vinicius Cesar <viniciusmaquine14@gmail.com>
2022-10-10 10:39:22 -0300

    Importação de Componentes PrimeVue

--- src/views/CriarTicket.vue
+++ src/views/CriarTicket.vue
@@ -1,61 +1,48 @@
 <template>
-  <div>
-    <Button label="Show" icon="pi pi-external-link" @click="openModal" />
-    <Dialog
-      header="Novo Ticket"
-      :visible.sync="displayModal"
-      :containerStyle="{ width: '50vw' }"
-      :modal="true"
-    >
-      <section>
-        <form>
-          <div class="create">
-            <div class="a">
-              <label for=""> Assunto </label>
-              <input
-                type="text"
-                id="assunto"
-                name="assunto"
-                placeholder=""
-                v-model="ticket.assunto"
-              />
-            </div>
-            <div>
-              <label for="">Tecnologia</label>
-              <input
-                type="text"
-                id="tecnologia"
-                placeholder=""
-                v-model="ticket.tecnologia"
-              />
-            </div>
-            <div>
-              <label for="">Colaboradores</label>
-              <select
-                name="colegas"
-                id="colegas"
-                v-model="ticket.colaboradores"
-              ></select>
-            </div>
+    <div>
+        <Button label="Show" icon="pi pi-external-link" @click="openModal" />
+        <Dialog header="Novo Ticket" :visible.sync="displayModal" :containerStyle="{ width: '50vw' }" :modal="true">
+            <section>
+                <form>
+                    <div class="create">
+                        <div class="a">
+                            <label for=""> Assunto </label>
+                            <input type="text" id="assunto" name="assunto" placeholder="" v-model="ticket.assunto" />
 
-            <div>
-              <label for=""> Data limite </label>
-              <Calendar v-model="ticket.dataLimite" />
-            </div>
+                        </div>
+                        <div>
+                            <label for="">Tecnologia</label>
 
-            <div>
-              <label for=""> Prioridade </label>
-              <select name="" id="" v-model="ticket.prioridade"></select>
-            </div>
-            <div>
-              <label for=""> Tipo</label>
-              <select name="" id="" v-model="ticket.tipo"></select>
-            </div>
-            <div class="enviar">
-              <button @click="enviarDados">Enviar</button>
-            </div>
-          </div>
-          <!-- <div class="ticket">
+
+                            <input type="text" id="tecnologia" placeholder="" v-model="ticket.tecnologia" />
+                        </div>
+                        <div>
+                            <label for="">Colaboradores</label>
+                            <select name="colegas" id="colegas" v-model="ticket.colaboradores"></select>
+                        </div>
+
+                        <div>
+                            <label for=""> Data limite </label>
+                            <Calendar v-model="ticket.dataLimite" />
+                        </div>
+
+                        <div>
+                            <label for=""> Prioridade </label>
+                            <Dropdown v-model="selectedCity" :options="cities" optionLabel="name"
+                                placeholder="Selecione" />
+                            <!-- <select name="" id="" v-model="ticket.prioridade"></select> -->
+                        </div>
+                        <div>
+                            <label for=""> Tipo</label>
+                            <Dropdown v-model="selectedCity" :options="types" optionLabel="name"
+                                placeholder="Selecione" />
+                            <!-- <select name="" id="" v-model="ticket.tipo"></select> -->
+                        </div>
+                        <div class="enviar">
+                            <button @click="enviarDados">Enviar</button>
+                        </div>
+                    </div>
+                    <!-- <div class="ticket">
                 <div class="minTop">
                     <p>Título-Título-Título-</p>
                     <p>000000000000</p>
@@ -64,21 +51,18 @@
                     <p>Tecnologia/Tecnologia</p>
                 </div>   
             </div> -->
-        </form>
-      </section>
-    </Dialog>
+                </form>
+            </section>
+        </Dialog>
 
-    <Button
-      label="No"
-      icon="pi pi-times"
-      @click="closeModal"
-      class="p-button-text"
-    />
-    <Button label="Yes" icon="pi pi-check" @click="closeModal" autofocus />
-  </div>
+        <Button label="No" icon="pi pi-times" @click="closeModal" class="p-button-text" />
+        <Button label="Yes" icon="pi pi-check" @click="closeModal" autofocus />
+    </div>
 </template>
 
 <script>
+import Dropdown from 'primevue/dropdown';
+import AutoComplete from 'primevue/autocomplete';
 import Calendar from "primevue/calendar";
 import Dialog from "primevue/dialog";
 import { getAuth } from "firebase/auth";
@@ -88,93 +72,121 @@ import { reactive, computed } from "vue";
 import { useVuelidate } from "@vuelidate/core";
 import { required, minLength } from "@vuelidate/validators";
 export default {
-  components: { Dialog, Calendar },
-  data() {
-    return {
-      displayModal: true
-    };
-  },
+    components: { Dialog, Calendar, AutoComplete, Dropdown },
+    data() {
+        return {
+            displayModal: true,
+            selectedCountry: null,
+            filteredCountries: null,
+            selectedCity: null,
+            cities: [
+                { name: 'Não Urgente' },
+                { name: 'Pouco Urgente' },
+                { name: 'Urgente' },
+                { name: 'Muito Urgente' },
+                { name: 'Emergência' }
+            ],
+            types: [
+                { name: 'UX/UI' },
+                { name: 'Desenvolvimento' },
+                { name: 'Engenharia' },
+                { name: 'Infra' },
 
-  setup() {
-    const ticket = reactive({
-      // pega todos os valores dos inputs do html
-      assunto: "",
-      tecnologia: "",
-      dataLimite: "",
-      fechamento: "",
-    });
-    const rules = computed(() => {
-      return {
-        // asssunto: { required, minLength: minLength(3) },
-        // tecnologia: { required, minLength: minLength(3) },
-        // dataLimite: { required }
-      };
-    });
-    const v$ = useVuelidate(rules, ticket);
-    return {
-      ticket,
-      v$,
-    };
-  },
-  methods: {
-    openModal() {
-      this.displayModal = true;
-    },
-    closeModal() {
-      this.displayModal = false;
-    },
-    enviarDados(e) {
-      e.preventDefault();
-      const auth = getAuth();
+            ]
 
-      this.v$.$validate();
-      if (!this.v$.$error) {
-        console.log("Vai salvar ", this.ticket);
-        this.saveOnDatabase();
-      } else {
-        console.log(this.v$);
-        alert("Observe os campos atentamente!!");
-      }
+        };
+    },
+    countryService: null,
+    created() {
+        this.countryService = new CountryService();
     },
 
-    async saveOnDatabase() {
-      const dbRef = collection(db, "ticket");
-      const data = {
-        assunto: this.ticket.assunto,
-        tecnologia: this.ticket.tecnologia,
-        // colaboradores: this.ticket.colaboradores,
-        // dataLimite: this.ticket.dataLimite,
-        // prioridade: this.ticket.prioridade,
-        // tipo: this.ticket.tipo,
-      };
-      console.log('dbref -> ', dbRef)
-      console.log('data -> ', data)
-      await addDoc(dbRef, data)
-        .then((docRef) => {
-          console.log("Document has been added successfully");
-        })
-        .catch((error) => {
-          console.log(error);
+    setup() {
+        const ticket = reactive({
+            // pega todos os valores dos inputs do html
+            assunto: "",
+            tecnologia: "",
+            dataLimite: "",
+            fechamento: "",
         });
+        const rules = computed(() => {
+            return {
+                // asssunto: { required, minLength: minLength(3) },
+                // tecnologia: { required, minLength: minLength(3) },
+                // dataLimite: { required }
+            };
+        });
+        const v$ = useVuelidate(rules, ticket);
+        return {
+            ticket,
+            v$,
+        };
     },
-  },
+    methods: {
+        openModal() {
+            this.displayModal = true;
+        },
+        closeModal() {
+            this.displayModal = false;
+        },
+        enviarDados(e) {
+            e.preventDefault();
+            const auth = getAuth();
+
+            this.v$.$validate();
+            if (!this.v$.$error) {
+                console.log("Vai salvar ", this.ticket);
+                this.saveOnDatabase();
+            } else {
+                console.log(this.v$);
+                alert("Observe os campos atentamente!!");
+            }
+        },
+        searchCountry(event) {
+            this.filteredCountriesBasic = this.countryService.search(event.query);
+        },
+
+        async saveOnDatabase() {
+            const dbRef = collection(db, "ticket");
+            const data = {
+                assunto: this.ticket.assunto,
+                tecnologia: this.ticket.tecnologia,
+                // colaboradores: this.ticket.colaboradores,
+                // dataLimite: this.ticket.dataLimite,
+                // prioridade: this.ticket.prioridade,
+                // tipo: this.ticket.tipo,
+            };
+            console.log('dbref -> ', dbRef)
+            console.log('data -> ', data)
+            await addDoc(dbRef, data)
+                .then((docRef) => {
+                    console.log("Document has been added successfully");
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+    },
 };
 </script>
 
+
+
 <style scoped>
 * {
-  margin: 0;
-  padding: 0;
+    margin: 0;
+    padding: 0;
 }
 
 section {
-  display: flex;
-  flex-direction: row;
-  height: 40vh;
-  width: 40vw;
-  /* grid-template-columns: 6fr 6fr;
+    display: flex;
+    flex-direction: row;
+    height: 40vh;
+    width: 40vw;
+    /* grid-template-columns: 6fr 6fr;
     grid-template-areas: "form ticket"; */
 }
+
 
 /* .create {
     grid-area: form;
@@ -191,57 +203,62 @@ section {
 } */
 
 form div {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 5px;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 5px;
 }
 
 form input {
-  outline: unset;
-  background-color: rgb(201, 200, 200);
-  border: none;
-  border-radius: 6px;
-  color: white;
-  padding: 0.25vh;
-  font-weight: bold;
+    outline: unset;
+    background-color: rgb(201, 200, 200);
+    border: none;
+    border-radius: 6px;
+    color: white;
+    padding: 0.25vh;
+    font-weight: bold;
 }
 
 form select {
-  outline: unset;
-  background-color: rgb(201, 200, 200);
-  border: none;
-  border-radius: 6px;
-  color: white;
-  padding: 0.25vh;
-  font-weight: bold;
+    outline: unset;
+    background-color: rgb(201, 200, 200);
+    border: none;
+    border-radius: 6px;
+    color: white;
+    padding: 0.25vh;
+    font-weight: bold;
 }
 
 .p-button {
-  margin: 0 0.5rem 0 0;
-  min-width: 10rem;
+    margin: 0 0.5rem 0 0;
+    min-width: 10rem;
 }
 
 p {
-  margin: 0;
+    margin: 0;
 }
 
 .confirmation-content {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .enviar {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 8vw;
-  padding-top: 2vh;
-  color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 8vw;
+    padding-top: 2vh;
+    color: white;
 }
 
 .enviar input {
-  background-color: #5c677d;
-  padding: 1vh;
+    background-color: #5c677d;
+    padding: 1vh;
+}
+</style>
+<style lang="scss" scoped>
+.p-dropdown-label {
+    overflow: inherit;
 }
 </style>
